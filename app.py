5777d466c23a16bc93af29c5bf409ec1ab79d43e
Hannah161jung <junghan1@students.zhaw.ch>
2026-03-18 10:33:05 +0100

    fehler korrigieren

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if 'data_df' not in st.session_state:
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
-pg_ph = st.Page("views/pH-Rechner.py", title="pH-Rechner", icon=":material/science:")
+pg_pH_Rechner = st.Page("views/pH_Rechner.py", title="pH-Rechner", icon=":material/science:")
 
-pg = st.navigation([pg_home, pg_ph])
+pg = st.navigation([pg_home, pg_pH_Rechner])
 pg.run()
